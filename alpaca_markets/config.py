2e5646a081b596c55b0f583ab06842aa371d8d80
Anthony Lieu <al6907@g.rit.edu>
2024-08-10 00:38:06 -0400

    move files to different folder

--- alpaca_markets/config.py
+++ alpaca_markets/config.py
@@ -4,5 +4,5 @@ API_SECRET = "hAI8OcHdSxnrCLchS7fZbfZHQHbac0MnvhGpzytv"
 ALPACA_CONFIG = {
     "API_KEY": "PKIW7PRBQM0V81UBAJIF",
     "API_SECRET": "hAI8OcHdSxnrCLchS7fZbfZHQHbac0MnvhGpzytv",
-    "ENDPOINT": "https://paper-api.alpaca.markets"
+    "PAPER": True
 }
