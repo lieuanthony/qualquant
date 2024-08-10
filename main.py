e27f7c185835d04b350d7d246b3f705a7d037d81
Anthony Lieu <al6907@g.rit.edu>
2024-08-09 23:09:41 -0400

    update symbols and keys and remove yfinance

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-import yfinance as yf
 from alpaca.trading.client import TradingClient
 from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.trading.requests import MarketOrderRequest
-from alpaca.trading.stream import TradingStream
+from alpaca.data import StockHistoricalDataClient, StocksTradeRequest
+from datetime import datetime
 import config
-
-def create_tickers() -> dict[yf.Ticker]:
-    tickers: dict[yf.Ticker] = dict()
-
-    with open("tickers.txt") as file:
-        for line in file:
-            ticker: yf.Ticker = yf.Ticker(line)
-            tickers[line] = ticker
-
-    return tickers
 
 def main() -> None:
     client: TradingClient = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
-    account = dict(client.get_account())
+
 
 if __name__ == "__main__":
     main()
