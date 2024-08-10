ac9a8e2d3106e55e9cffa6ba2a2adb426df7bdc5
Anthony Lieu <al6907@g.rit.edu>
2024-08-09 23:59:54 -0400

    restart approach, edit config file

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 API_KEY = "PKIW7PRBQM0V81UBAJIF"
-SECRET_KEY = "hAI8OcHdSxnrCLchS7fZbfZHQHbac0MnvhGpzytv"
+API_SECRET = "hAI8OcHdSxnrCLchS7fZbfZHQHbac0MnvhGpzytv"
+
+ALPACA_CONFIG = {
+    "API_KEY": "PKIW7PRBQM0V81UBAJIF",
+    "API_SECRET": "hAI8OcHdSxnrCLchS7fZbfZHQHbac0MnvhGpzytv",
+    "ENDPOINT": "https://paper-api.alpaca.markets"
+}
