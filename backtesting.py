b0250fe4801372b78617b14e0b050602a9de683d
Anthony Lieu <al6907@g.rit.edu>
2024-08-18 13:26:33 -0400

    conduct more backtests with different years and edit strat

--- backtesting.py
+++ backtesting.py
@@ -7,6 +7,15 @@ def main() -> None:
     backtesting_start = datetime(2023, 8, 15)
     backtesting_end = datetime(2024, 8, 15)
 
+    # backtesting_start = datetime(2022, 8, 15)
+    # backtesting_end = datetime(2023, 8, 15)
+
+    # backtesting_start = datetime(2021, 8, 15)
+    # backtesting_end = datetime(2022, 8, 15)
+
+    # backtesting_start = datetime(2020, 8, 15)
+    # backtesting_end = datetime(2021, 8, 15)
+
     trader = Trader(backtest=True)
     data_source = YahooDataBacktesting(
         datetime_start=backtesting_start,
